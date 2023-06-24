f318d2946e33d418ab31ecddbf2884bf5e8550ca
BasilZap <zapr9@yandex.ru>
2023-06-24 04:18:48 +0300

    Проверка работы API hh

--- src/api_classes.py
+++ src/api_classes.py
@@ -1,7 +1,19 @@
 import requests
+import json
+import time
 from abc import ABC, abstractmethod
 
 # 1. Создать абстрактный класс для работы с API сайтов с вакансиями
 # 2. Реализовать классы, наследующиеся от абстрактного класса, для работы с конкретными платформами
 
-class GetAPIdata(ABC):
+
+params = {
+        'text': 'NAME:Python', # Текст фильтра. В имени должно быть слово "Аналитик"
+        'area': 113, # Поиск осуществляется по вакансиям города Москва
+        'page': 0, # Индекс страницы поиска на HH
+        'per_page': 100 # Кол-во вакансий на 1 странице
+    }
+
+rec = requests.get("https://api.hh.ru/vacancies", params)
+print(rec)
+print(rec.content.decode())
