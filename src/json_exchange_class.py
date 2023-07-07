b3171c7d3d9d20e955a3af8bd044bcf954ee053b
BasilZap <zapr9@yandex.ru>
2023-07-08 02:42:45 +0300

    Из класса JSONSaver убраны все методы, кроме сохранения json-данных в файл и их выгрузки из файла.

--- src/json_exchange_class.py
+++ src/json_exchange_class.py
@@ -14,18 +14,10 @@ JSON_FILENAME = 'vacancy_data.json'
 class JsonAbstract(ABC):
 
     @abstractmethod
-    def save_raw_to_json(self):
+    def save_raw_to_json(self, vacancies_data):
         pass
 
     def save_vacancy_to_json(self):
-        pass
-
-    @abstractmethod
-    def delete_vacancy(self, vacancy_id):
-        pass
-
-    @abstractmethod
-    def get_vacancy_by_salary(self):
         pass
 
 
@@ -33,14 +25,15 @@ class JSONSaver(JsonAbstract):
     """
     Инициализация класса JSONSaver
     """
-    def __init__(self, vacancies: list):
-        self.vacancies = vacancies
+    def __init__(self):
+        self.vacancies = []
 
-    def save_raw_to_json(self) -> None:
+    def save_raw_to_json(self, vacancies_data) -> None:
         """
         Запись данных от API в файл 'vacancy_data.json'
         :return: -> None
         """
+        self.vacancies = vacancies_data
         with open('vacancy_data.json', 'w', encoding='utf-8') as file:
             json.dump(self.vacancies, file, ensure_ascii=False)
             print(f'Вакансии сохранены в файл vacancy_data.json')
@@ -53,66 +46,20 @@ class JSONSaver(JsonAbstract):
             self.vacancies = vacancies_data
         file.close()
 
-    def save_vacancy_to_json(self) -> list:
-        """
-        Собираем данные из экземпляров класса Vacancy
-        в json и возвращаем списком
-        :return: список вакансий json
-        """
-        vacancy_list = []
-        for recs in Vacancy.all:
-            vacancy_list.append(recs.get_json_from_vacancy())
-            print(f'{len(vacancy_list)} вакансий готовы к сохранению в файл')
-        return vacancy_list
 
-    def get_vacancies(self) -> None:
-        """
-        Статический метод создания экземпляров класса
-        Vacancy, инициализируемых данными из файла
-        """
-        Vacancy.all.clear()
-        vacancies_data = self.vacancies
-        for it in vacancies_data:
-            Vacancy(it['id'], it['name'], it['url'], it['salary_from'], it['salary_to'], it['description'],
-                    it['company'], it['api'])
-
-    def delete_vacancy(self, vacancy_id):
-        with open('vacancy_data.json', 'r', encoding='utf-8') as file:
-            vacancies_data = json.load(file)
-        file.close()
-        new_data = []
-        list_length = len(vacancies_data)
-        for vacancy in vacancies_data:
-            if vacancy_id != vacancy['id']:
-                new_data.append(vacancy)
-        with open('vacancy_data.json', 'w', encoding='utf-8') as file:
-            json.dump(new_data, file, ensure_ascii=False)
-        file.close()
-        if list_length == len(new_data):
-            print('Запись с таким id не найдена')
-        else:
-            print('Запись удалена')
-
-    def get_vacancy_by_salary(self):
-        with open('vacancy_data.json', 'r', encoding='utf-8') as file:
-            vacancies_data = json.load(file)
-            self.vacancies = sorted(vacancies_data, key=lambda vacancy: int(vacancy['salary_from']), reverse=True)
-        file.close()
-
-
-ap1 = HeadHunterAPI()
-ap1.find_area_id('Москва')  # Выбираем где искать
-ap1.get_api_data('python')  # Выбираем что искать
-data = ap1.get_vacancies()  # Получаем данные в json формате
-js = JSONSaver(data)    # Создаем экземпляр json_saver, сохраняем данные от api в файл
-js.save_raw_to_json()
-js.get_data_from_file()
+#ap1 = HeadHunterAPI()
+#ap1.find_area_id('Москва')  # Выбираем где искать
+#ap1.get_api_data('python')  # Выбираем что искать
+#data = ap1.get_api_vacancy_json_list()  # Получаем данные в json формате
+#js = JSONSaver(data)    # Создаем экземпляр json_saver, сохраняем данные от api в файл
+#js.save_raw_to_json()
+#js.get_data_from_file()
 #js.get_vacancies()  # Добавляем данные из файла в класс Vacancies
 # vac1 = Vacancy('123458', 'Разраб2', 'http:/1.ru', '1000', '10000', 'Оч хорошая, не напряжная такая, сойдет',
                 # 'A&B', 'hh.ru')
-js.get_vacancy_by_salary()
-js.get_vacancies()
-for vacs in range(len(Vacancy.all)):    # Вывод всех вакансий
-    print(repr(Vacancy.all[vacs]))
-js.delete_vacancy('80643834')
+#js.get_vacancy_by_salary()
+#js.get_vacancies()
+#for vacs in range(len(Vacancy.all)):    # Вывод всех вакансий
+    #print(repr(Vacancy.all[vacs]))
+#js.delete_vacancy('80643834')
 # print(js.save_vacancy_to_json())
