f348487f75b752b1273840f809d6fb433c846cac
BasilZap <zapr9@yandex.ru>
2023-07-10 03:58:41 +0300

    В main.py добавлены описания функций добавлен пункт "f"3 - Дополнительный поиск по ключевому слову или фразе""

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ from src.api_classes import HeadHunterAPI, SuperJobAPI
 from src.json_exchange_class import JSONSaver
 from src.vacancy_classes import Vacancy
 
-# 6. Создать функцию для взаимодействия с пользователем
 hh_api = HeadHunterAPI()
 sj_api = SuperJobAPI()
 j_saver = JSONSaver()
 vacancy = Vacancy
 
 
-def user_interaction():
-
+def user_interaction() -> None:
+    """
+    Экран выбора api для пользователя
+    :return: None
+    """
     while True:
         print(f'==================================================================================')
         print(f'Добро пожаловать в программу работы с вакансиями с сайтов HeadHunter и SuperJob!!!')
@@ -38,7 +40,11 @@ def user_interaction():
                 exit()
 
 
-def work_with_hh():
+def work_with_hh() -> None:
+    """
+    Функция работы с API HeadHunter
+    :return: None
+    """
     profession = input('Ведите название вакансии:\n')
     area = input('Ведите город в котором нужно искать или нажмите Enter - поиск по России:\n')
     hh_api.find_area_id(area)
@@ -48,7 +54,11 @@ def work_with_hh():
     vacancy.get_vacancies(j_saver.vacancies)
 
 
-def work_with_sj():
+def work_with_sj() -> None:
+    """
+    Функция работы с API SuperJob
+    :return: None
+    """
     profession = input('Ведите название вакансии:\n')
     area = input('Ведите город в котором нужно искать или нажмите Enter - поиск по России:\n')
     sj_api.area = area
@@ -58,7 +68,11 @@ def work_with_sj():
     vacancy.get_vacancies(j_saver.vacancies)
 
 
-def work_with_both_platforms():
+def work_with_both_platforms() -> None:
+    """
+    Функция работы с API обеих платформ
+    :return: None
+    """
     profession = input('Ведите название вакансии:\n')
     area = input('Ведите город в котором нужно искать или нажмите Enter - поиск по России:\n')
     hh_api.find_area_id(area)
@@ -72,18 +86,19 @@ def work_with_both_platforms():
     vacancy.get_vacancies(j_saver.vacancies)
 
 
-def second_menu():
+def second_menu() -> None:
     while True:
         print("=======================================================================================")
         print(f"Работа с вакансиями, Ваши действия:")
         print(f"1 - Показать несколько вакансий или все вакансии")
-        print(f"2 - Сортировать вакансии по минимальной зарплате")
-        print(f"3 - Добавить вакансию")
-        print(f"4 - Удалить вакансию")
-        print(f"5 - Сохранить вакансии в файл")
-        print(f"6 - Вернуться на первый экран")
+        print(f"2 - Сортировать вакансии по минимальной зарплате и показать ТОП")
+        print(f"3 - Дополнительный поиск по ключевому слову или фразе")
+        print(f"4 - Добавить вакансию")
+        print(f"5 - Удалить вакансию")
+        print(f"6 - Сохранить вакансии в файл")
+        print(f"7 - Вернуться на первый экран")
         answer = input('Введите Exit для выхода\n')
-        if answer.lower() in ("1", "2", "3", "4", "5", "6", "exit"):
+        if answer.lower() in ("1", "2", "3", "4", "5", "6", "7", "exit"):
             if answer == "1":
                 vac_number = input("Введите количество вакансий для вывода, или нажмите Enter - показать все вакансии\n")
                 if vac_number.isdigit():
@@ -96,9 +111,19 @@ def second_menu():
                 vacancies = vacancy.get_json_from_vacancy()
                 sorted_vacancies = vacancy.get_vacancy_by_salary(vacancies)
                 vacancy.get_vacancies(sorted_vacancies)
-                vacancy.show_n_vacancies()
+                vacancies_for_show = input('Введите число ТОП-вакансий для показа\n')
+                if vacancies_for_show.isdigit():
+                    vacancy.show_n_vacancies(int(vacancies_for_show))
+                elif vacancies_for_show == '':
+                    vacancy.show_n_vacancies()
+                else:
+                    print('Некорректный ввод, повторите попытку\n')
 
             elif answer == "3":
+                word = input("Введите слово или фразу\n")
+                vacancy.search_additional_words(word)
+
+            elif answer == "4":
                 v_id = input("Введите ID вакансии\n")
                 name = input("Введите название вакансии\n")
                 link = input("Введите ссылку на вакансию\n")
@@ -108,12 +133,12 @@ def second_menu():
                 company = input("Введите название компании\n")
                 vacancy.add_vacancy(v_id, name, link, salary_from, salary_to, description, company)
                 print(f'Вакансия {repr(vacancy.all[-1])} успешно добавлена')
-            elif answer == "4":
+            elif answer == "5":
                 vacancy_to_del = input('Введите ID вакансии для удаления:\n')
                 vacancy.delete_vacancy(vacancy_to_del)
-            elif answer == "5":
-                j_saver.save_raw_to_json(vacancy.get_json_from_vacancy())
             elif answer == "6":
+                j_saver.save_raw_to_json(vacancy.get_json_from_vacancy())
+            elif answer == "7":
                 user_interaction()
             else:
                 exit()
@@ -121,4 +146,5 @@ def second_menu():
             print('Некорректный ввод, проверьте раскладку клавиатуры и введите один из предлагаемых вариантов\n')
 
 
-user_interaction()
+if __name__ == "__main__":
+    user_interaction()
